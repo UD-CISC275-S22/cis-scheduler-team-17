ea5f3322a3467cbc03d05a585dac3f2391cdd431
rfilano <rfilano6@gmail.com>
2022-04-06 18:00:58 -0400

    Added Welcome Message

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>Zoe Valladares</p>
-            <p>Rosemarie Filano</p>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <header className="App-header">Schedule Planner</header>
+            <div>
+                Welcome to Schedule Planner! Click a current plan or create a
+                new plan to get started.
+            </div>
+            <div>
+                <p>Rosemarie Filano, Zoe Valladares, Sydney Hester</p>
+                <p>Team 17 Final Project</p>
+            </div>
         </div>
     );
 }
