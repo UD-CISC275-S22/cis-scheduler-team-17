5aa36333afcb3381a74066dfab00fb5930795f67
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-06 12:46:05 -0400

    made courses interface -- just added one attribute

---
+++ src/interfaces/courses.ts
@@ -0,0 +1,12 @@
+/**
+ * Courses
+ * - name, string
+ * - credits, number
+ * - taken, boolean
+ * - description, string
+ * - preReqs, list of strings
+ */
+
+export interface Courses {
+    course_name: string;
+}
