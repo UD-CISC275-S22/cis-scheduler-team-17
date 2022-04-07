bf335ba5708ec1dffe96aa566b25535d8d32c011
rfilano <rfilano6@gmail.com>
2022-04-07 19:46:08 -0400

    Moved buttons + other small edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
 import "./App.css";
 import { UnderConstruction } from "./components/Underconstruction";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">CISC Schedule Planner</header>
+            <header className="App-header">
+                Computer & Information Sciences Schedule Planner
+            </header>
             <div className="Welcome-Message">
                 Welcome to CISC Schedule Planner. <br></br>To get started,
                 please click your desired degree.
             </div>
             <UnderConstruction></UnderConstruction>
+            <div>
+                <Row className="buttons">
+                    <Col>
+                        <Button className="BA">Computer Science BA</Button>
+                    </Col>
+                    <Col>
+                        <Button className="BS">Computer Science BS</Button>
+                    </Col>
+                    <Col>
+                        <Button className="IS">Information Systems BS</Button>
+                    </Col>
+                </Row>
+            </div>
             <footer className="footer">
                 Constributers: Team 17
                 <br></br>Rosemarie Filano, Sydney Hester, Zoe Valladares
