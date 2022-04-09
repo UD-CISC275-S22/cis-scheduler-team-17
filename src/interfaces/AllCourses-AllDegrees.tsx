dee034472b53f4bea51b9b3e6af322cc046aacd7
Sydney <sydneyph@udel.edu>
2022-04-09 16:16:07 -0400

    Hard coded the first semester of courses for the 2 year plan

--- src/interfaces/AllCourses-AllDegrees.tsx
+++ src/interfaces/AllCourses-AllDegrees.tsx
@@ -1,4 +1,5 @@
 import { makeCourse, makeDegree } from "./makeDegree-Makecourses";
+import { Season } from "./course-Degree-Semester";
 
 const allCourses = [
     makeCourse("name", "description", ["Spring", "Fall"], [], 0)
@@ -7,3 +8,70 @@ console.log(allCourses);
 
 const AllDegrees = [makeDegree("null", "null", [], 0)];
 console.log(AllDegrees);
+// the hard coded degrees will go here
+
+//These are all of the hard coded courses
+const EGGG101 = makeCourse(
+    "EGGG101: Introduction to Engineering",
+    "Introduction to profession, including disciplines of chemical, civil, computer, electrical, environmental, and mechanical engineering. Prepares students for success through integration of: technical problem solving and engineering design, ethical decision-making, teamwork, and communicating to diverse audiences.",
+    ["Fall"],
+    [],
+    2
+);
+const CISC108 = makeCourse(
+    "CISC108: Introduction To Computer Science 1",
+    "Computing and principles of programming with an emphasis on systematic program design. Topics include functional programming, data abstraction, procedural abstraction, use of control and state, recursion, testing, and object-oriented programming concepts. Requires no prior programming experience, open to any major, but intended primarily for majors and minors in computer science or mathematics. COREQ: MATH115, MATH117, or higher math course or math placement. RESTRICTIONS: Students who received credit in CISC106 are not eligible to take this course without permission.",
+    ["Fall", "Spring", "Summer"],
+    [],
+    3
+);
+const MATH241 = makeCourse(
+    "MATH241: Analytic Geometry and Calculus A",
+    "Functions, limits, continuity, derivatives. Polynomial, rational, exponential, hyperbolic, logarithmic, trigonometric and inverse trigonometric functions. Definite and indefinite integrals and the Fundamental Theorem of Calculus. Simple differential equations (separable ODE, linear ODE). ODE models leading to exponential growth and decay.",
+    ["Fall", "Winter", "Spring", "Summer"],
+    [],
+    4
+);
+const ENGL110 = makeCourse(
+    "ENGL110: Seminar in Composition",
+    "Introduction to the process of academic writing that centers on the composition of analytical, research-based essays.",
+    ["Fall", "Winter", "Spring", "Summer"],
+    [],
+    3
+);
+const BreadthRequirementCAAH = makeCourse(
+    "Breadth Requirement Creative Arts and Humanities",
+    "These courses provide students with an understanding and appreciation of the visual and performing arts, of aesthetic forms, designs, or craftsmanship, or of literary, philosophical, and intellectual traditions. Courses may focus on a single aesthetic form or intellectual tradition, or cross-cultural comparisons.",
+    ["Fall", "Winter", "Spring", "Summer"],
+    [],
+    3
+);
+const BreadthRequirementHACC = makeCourse(
+    "Breadth Requirement History and Cultural Change",
+    "These courses provide students with an understanding of the sources and forces of historical changes in ideas, beliefs, institutions, and cultures. Courses may address social, cultural, intellectual, economic, technological, artistic, scientific, and political development, changes in a discipline, or globalization and its effects.",
+    ["Fall", "Winter", "Spring", "Summer"],
+    [],
+    3
+);
+const BreadthRequirementSABS = makeCourse(
+    "Breadth Requirement Social and Behavioral Sciences",
+    "These courses provide students with an understanding of the behavior of individuals and social groups in the context of their human and natural environments. Courses emphasize the empirical findings, applications, and methods of the social and behavioral sciences.",
+    ["Fall", "Winter", "Spring", "Summer"],
+    [],
+    3
+);
+const BreadthRequirementGen = makeCourse(
+    "Breadth Requirement General",
+    "A college education requires some breadth of knowledge across diverse fields and perspectives. With this in mind, all students are required to complete a minimum of 12 credits from the list of University breadth courses. This includes 3 credits from each of the following categories. Students must earn a minimum grade of C- in each course to meet this requirement",
+    ["Fall", "Winter", "Spring", "Summer"],
+    [],
+    3
+);
+const CISC181 = makeCourse(
+    "CISC181: Introduction to Computer Science II",
+    "Principles of computer science illustrated and applied through programming in an object oriented language. Programming projects illustrate computational problems, styles and issues that arise in computer systems development and in all application areas of computation.",
+    ["Fall", "Spring"],
+    [CISC108, MATH241],
+    3
+);
+const CISC210 = makeCourse("CISC210: Introduction to Systems Programming", );
