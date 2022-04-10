2007efb6f7848005ca8045b941fc793fbfbde046
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-09 22:11:34 -0400

    fixed linting

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                sydneyph
-            </p>
+            <p>sydneyph</p>
         </div>
     );
 }
