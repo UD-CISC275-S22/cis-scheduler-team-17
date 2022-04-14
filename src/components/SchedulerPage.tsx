589dcec02b8493c3528b15e1ed2bf120eb708de0
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-14 14:25:30 -0400

    added hardcoded table

--- src/components/SchedulerPage.tsx
+++ src/components/SchedulerPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import "../App.css";
 //import Homepage from "./Homepage";
 
@@ -8,6 +8,27 @@ export function SchedulerPage({
 }: {
     changeHomepage: () => void;
 }): JSX.Element {
+    function showSemester(): JSX.Element {
+        return (
+            <div>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th> Semester + Year </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                {/* <Button onClick={() => deleteSemester()}></Button> */}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tr> Course ID </tr>
+                    <tr> Number of Credits </tr>
+                    <tbody>All of the data</tbody>
+                </Table>
+            </div>
+        );
+    }
     return (
         <div className="App">
             <header className="App-header">
@@ -16,6 +37,7 @@ export function SchedulerPage({
             <div>
                 <h3>You are planning a [] degree</h3>
             </div>
+            <div>{showSemester()}</div>
             <footer className="footer">
                 <Button onClick={changeHomepage}>Back</Button>
             </footer>
