c2ef1964189d7a38eca33389c77b9c52720444c0
rfilano <rfilano6@gmail.com>
2022-04-14 14:57:40 -0400

    Added Degree Descriptions

--- src/components/DropdownDegrees.tsx
+++ src/components/DropdownDegrees.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { AllDegrees } from "../interfaces/AllCourses-AllDegrees";
 import { Degree } from "../interfaces/course-Degree-Semester";
 import "../App.css";
-import { Form, Tooltip } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 type ChangeEvent = React.ChangeEvent<
     HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
@@ -10,9 +10,15 @@ type ChangeEvent = React.ChangeEvent<
 
 export function DropdownDegrees(): JSX.Element {
     const degrees = [...AllDegrees];
-    const [degree, setDegree] = useState<string>(degrees[0].name);
+    const [degree, setDegree] = useState<Degree>(degrees[0]);
     function updateDegree(event: ChangeEvent) {
-        setDegree(event.target.value);
+        const newDegreeName = event.target.value;
+        const newDegree = degrees.find(
+            (degree: Degree): boolean => degree.name === newDegreeName
+        );
+        if (newDegree !== undefined) {
+            setDegree(newDegree);
+        }
     }
     return (
         <div>
@@ -20,7 +26,7 @@ export function DropdownDegrees(): JSX.Element {
                 <Form.Label>Degrees</Form.Label>
                 <Form.Select
                     className="dropdownForm"
-                    value={degree}
+                    value={degree.name}
                     onChange={updateDegree}
                 >
                     {degrees.map((degree: Degree) => (
@@ -34,6 +40,7 @@ export function DropdownDegrees(): JSX.Element {
                     ))}
                 </Form.Select>
             </Form.Group>
+            <span>{degree.description}</span>
         </div>
     );
 }
