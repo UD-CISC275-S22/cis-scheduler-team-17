dee9810c781017cc963dea9e175ac8cc3ffb9b10
Sydney <sydneyph@udel.edu>
2022-04-24 15:47:09 -0400

    We have added the taken course list and the not taken course list. It updates based on the classes and we can move back and forht to update it.

--- src/components/SchedulerPage.tsx
+++ src/components/SchedulerPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Button, Col, Container, Form, Row, Table } from "react-bootstrap";
 import "../App.css";
-import { Season } from "../interfaces/course-Degree-Semester";
+import { Course, Degree, Season } from "../interfaces/course-Degree-Semester";
+import { currentSelectedDegree } from "./DropdownDegrees";
 //import Homepage from "./Homepage";
 //interfaces
 //import { AllDegrees } from "../interfaces/AllCourses-AllDegrees";
@@ -85,7 +86,9 @@ export function SchedulerPage({
                             <div> {addSemester()} </div>
                         </Col>
                         <Col>
-                            <span> Courses </span>
+                            <span>
+                                <CoursesLists></CoursesLists>
+                            </span>
                         </Col>
                     </Row>
                     <Row>
@@ -101,6 +104,130 @@ export function SchedulerPage({
             </footer>
         </div>
     );
+}
+
+function CoursesLists(): JSX.Element {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div>Courses You Have Taken</div>
+                        <PrintDegreesLists taken={true}></PrintDegreesLists>
+                    </Col>
+                    <Col>
+                        <div>Courses You Have Not Taken</div>
+                        <PrintDegreesLists taken={false}></PrintDegreesLists>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+function PrintDegreesLists({ taken }: { taken: boolean }): JSX.Element {
+    // this is going to be where the courses are printed
+    UseYellows();
+    const [currentDegree, setDegree] = useState<Degree>(currentSelectedDegree);
+    const [currentCourseName, setCurrentCourseName] = useState<string>(
+        currentSelectedDegree.CoursesRequired[0].name
+    );
+    const [printCourses, setPrintTakenOrNot] = useState<Course[]>(
+        currentDegree.CoursesRequired.filter(
+            (course: Course): boolean => course.taken === taken
+        )
+    );
+    const [progress, setProgress] = useState(0);
+    const [currentTaken, setCurrentTaken] = useState<boolean>();
+
+    function UseYellows() {
+        // this is only here to get ris of the yellows in the code
+        // eslint-disable-next-line no-constant-condition
+        if (!true) {
+            console.log(currentCourseName);
+            console.log(currentTaken);
+        }
+    }
+
+    function updateList() {
+        setDegree(currentSelectedDegree);
+        // updating our list
+        setPrintTakenOrNot(
+            currentDegree.CoursesRequired.filter(
+                (course: Course): boolean => course.taken === taken
+            )
+        );
+    }
+
+    const scrollHandler = (event: React.UIEvent<HTMLDivElement>) => {
+        // this handles the scrolling of the box
+        const containerHeight = event.currentTarget.clientHeight;
+        const scrollHeight = event.currentTarget.scrollHeight;
+
+        const scrollTop = event.currentTarget.scrollTop;
+        setProgress(((scrollTop + containerHeight) / scrollHeight) * 100);
+        setDegree(currentSelectedDegree);
+        // updating our list
+        setPrintTakenOrNot(
+            currentDegree.CoursesRequired.filter(
+                (course: Course): boolean => course.taken === taken
+            )
+        );
+    };
+
+    return (
+        <div>
+            <Button onClick={updateList}>Refresh</Button>
+            <div style={styles.container} onScroll={scrollHandler}>
+                {printCourses.map((currentCourse: Course) => (
+                    <div key={currentCourse.name}>
+                        Course ID: {currentCourse.courseID}
+                        {currentCourse.taken_String}
+                        <br></br>
+                        Pre Requisite:
+                        {currentCourse.prerecs.map(
+                            (currentPreRec: Course) =>
+                                currentPreRec.courseID +
+                                " " +
+                                currentPreRec.taken_String +
+                                "\n"
+                        )}
+                        <br></br>
+                        ____________
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+// Styling
+const styles = {
+    container: {
+        width: 275,
+        height: 300,
+        margin: "30px auto",
+        overflowY: "auto",
+        overflowX: "hidden",
+        background: "mintcream"
+    },
+    list: {
+        width: "100%"
+    },
+    progressBar: {
+        width: 600,
+        height: 20,
+        margin: "auto",
+        backgroundColor: "#bbb"
+    },
+    progressValue: {
+        height: "100%",
+        backgroundColor: "steelblue"
+    },
+    text: {
+        textAlign: "center"
+    }
+} as const;
+{
+    /** Code for the scrolly box and the progress bar used from https://www.kindacode.com/article/react-typescript-handling-onscroll-event/*/
 }
 
 export default SchedulerPage;
