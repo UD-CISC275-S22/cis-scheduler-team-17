7c1e600c055331640604697a457444bcb2aad56a
rfilano <rfilano6@gmail.com>
2022-04-30 23:51:26 -0400

    Fixed some errors

--- src/components/ExportCSV.tsx
+++ src/components/ExportCSV.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { CSVLink } from "react-csv";
-import { SemesterPlanner } from "../interfaces/course-Degree-Semester";
+import { Degree } from "../interfaces/course-Degree-Semester";
 import "../App.css";
 
-export function ExportCSV({
-    semesters
-}: {
-    semesters: SemesterPlanner[];
-}): JSX.Element {
-    const csvData = semesters;
+export function ExportCSV({ degree }: { degree: Degree }): JSX.Element {
+    const csvData = degree.SemesterList;
     const csvHeaders = [
         { label: "Season", key: "SemesterSeason" },
         { label: "Year", key: "year" },
