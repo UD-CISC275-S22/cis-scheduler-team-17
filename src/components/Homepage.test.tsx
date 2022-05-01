3cb1957b54d50d95ba7870f8284db1956d9f25bb
Sydney <sydneyph@udel.edu>
2022-05-01 14:04:00 -0400

    Have some basic tests for the select taken done

--- src/components/Homepage.test.tsx
+++ src/components/Homepage.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { AllCourses } from "../interfaces/AllCourses-AllDegrees";
+
 describe("Testing everything on the homepage", () => {
     describe("Testing the Select Taken UI", () => {
         beforeEach(() => {
@@ -14,36 +14,48 @@ describe("Testing everything on the homepage", () => {
                 )
             ).toBeInTheDocument;
         });
-        test("Testing to see if a degree name has been printed", () => {
-            expect(screen.getByText(/{AllCourses[0].name}/)).toBeInTheDocument;
+        test("Testing to see if the first degree name has been printed", () => {
+            expect(screen.getByText(/EGGG101: Introduction to Engineering/i))
+                .toBeInTheDocument;
         });
         test("Testing to see if taken starts off as false", () => {
             expect(
                 screen.getByText(
-                    "Please Select The Courses You Have Already Taken"
+                    /Course Name: EGGG101: Introduction to Engineering Course Credits: 2 ...... Pre Requisite: ...... Taken: ❌/i
                 )
             ).toBeInTheDocument;
         });
-        test("testing to see if there is a checkbox identified with a course name", () => {
-            expect(
-                screen.getByText(
-                    "Please Select The Courses You Have Already Taken"
-                )
-            ).toBeInTheDocument;
+        test("testing to see if there is a list of checkboxes present on the page", () => {
+            const checkboxi: HTMLInputElement[] =
+                screen.getAllByRole("checkbox");
+            checkboxi[0].click();
+            checkboxi[0].click();
+            checkboxi[1].click();
+            checkboxi[1].click();
+            const length = checkboxi.length;
+            checkboxi[length - 1].click();
+            checkboxi[length - 1].click();
         });
         test("Testing to see if the user can check the check box and change the status of taken", () => {
-            expect(
-                screen.getByText(
-                    "Please Select The Courses You Have Already Taken"
-                )
-            ).toBeInTheDocument;
+            const checkboxes: HTMLInputElement[] =
+                screen.getAllByRole("checkbox");
+            checkboxes[0].click();
+            screen.getByText(
+                /Course Name: EGGG101: Introduction to Engineering Course Credits: 2 ...... Pre Requisite: ...... Taken: ✔️/i
+            );
         });
         test("Testing to see if changing the status of taken twice gets us back to not taken", () => {
-            expect(
-                screen.getByText(
-                    "Please Select The Courses You Have Already Taken"
-                )
-            ).toBeInTheDocument;
+            const checkboxes: HTMLInputElement[] =
+                screen.getAllByRole("checkbox");
+            checkboxes[0].click();
+            checkboxes[0].click();
+            screen.getByText(
+                /Course Name: EGGG101: Introduction to Engineering Course Credits: 2 ...... Pre Requisite: ...... Taken: ✔️/i
+            );
+            checkboxes[0].click();
+            screen.getByText(
+                /Course Name: EGGG101: Introduction to Engineering Course Credits: 2 ...... Pre Requisite: ...... Taken: ❌/i
+            );
         });
     });
 });
