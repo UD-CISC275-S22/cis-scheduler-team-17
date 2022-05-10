d07ccec21472646ce4f5b38ead5f1806603494ac
Sydney <sydneyph@udel.edu>
2022-05-10 14:28:27 -0400

    fixed my tests (removed refresh)

--- src/Tests/SchedulerPage.test.tsx
+++ src/Tests/SchedulerPage.test.tsx
@@ -50,10 +50,6 @@ describe("Testing aspects of the ", () => {
             const SchedulerButton: HTMLInputElement =
                 screen.getByText("Make Schedule");
             SchedulerButton.click();
-            const RefreshX: HTMLInputElement = screen.getByText("Refresh ❌");
-            const RefreshY: HTMLInputElement = screen.getByText("Refresh ✔️");
-            RefreshX.click();
-            RefreshY.click();
             expect(screen.getByText(/Course ID: EGGG101✔️Pre Requisite:/i))
                 .toBeInTheDocument;
         });
@@ -64,10 +60,6 @@ describe("Testing aspects of the ", () => {
             const SchedulerButton: HTMLInputElement =
                 screen.getByText("Make Schedule");
             SchedulerButton.click();
-            const RefreshX: HTMLInputElement = screen.getByText("Refresh ❌");
-            const RefreshY: HTMLInputElement = screen.getByText("Refresh ✔️");
-            RefreshX.click();
-            RefreshY.click();
             expect(screen.findByText(/Course ID: EGGG101✔️Pre Requisite:/i))
                 .toBeInTheDocument;
             // we have made sure thet we get the proper output the first time around, going back to update the input again
@@ -75,8 +67,6 @@ describe("Testing aspects of the ", () => {
             Back.click();
             checkbox[0].click;
             SchedulerButton.click();
-            RefreshX.click();
-            RefreshY.click();
             expect(screen.findByText(/Course ID: EGGG101❌Pre Requisite:/i))
                 .toBeInTheDocument;
         });
@@ -84,10 +74,6 @@ describe("Testing aspects of the ", () => {
             const SchedulerButton: HTMLInputElement =
                 screen.getByText("Make Schedule");
             SchedulerButton.click();
-            const RefreshX: HTMLInputElement = screen.getByText("Refresh ❌");
-            const RefreshY: HTMLInputElement = screen.getByText("Refresh ✔️");
-            RefreshX.click();
-            RefreshY.click();
             expect(
                 screen.getByText(
                     /Course ID: CISC181❌Pre Requisite:CISC108 ❌ MATH241 ❌/i
@@ -102,10 +88,6 @@ describe("Testing aspects of the ", () => {
             const SchedulerButton: HTMLInputElement =
                 screen.getByText("Make Schedule");
             SchedulerButton.click();
-            const RefreshX: HTMLInputElement = screen.getByText("Refresh ❌");
-            const RefreshY: HTMLInputElement = screen.getByText("Refresh ✔️");
-            RefreshX.click();
-            RefreshY.click();
             expect(
                 screen.getByText(
                     /Course ID: CISC181❌Pre Requisite:CISC108 ✔️ MATH241 ✔️/i
@@ -120,10 +102,6 @@ describe("Testing aspects of the ", () => {
             const SchedulerButton: HTMLInputElement =
                 screen.getByText("Make Schedule");
             SchedulerButton.click();
-            const RefreshX: HTMLInputElement = screen.getByText("Refresh ❌");
-            const RefreshY: HTMLInputElement = screen.getByText("Refresh ✔️");
-            RefreshX.click();
-            RefreshY.click();
             expect(
                 screen.findByText(
                     /Course ID: CISC181❌Pre Requisite:CISC108 ✔️ MATH241 ✔️/i
@@ -135,8 +113,6 @@ describe("Testing aspects of the ", () => {
             checkboxi[1].click();
             checkboxi[2].click();
             SchedulerButton.click();
-            RefreshX.click();
-            RefreshY.click();
             expect(
                 screen.findByText(
                     /Course ID: CISC181❌Pre Requisite:CISC108 ❌ MATH241 ❌/i
